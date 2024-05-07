842277b20cd520ad629d247f800a7c4605a6a8bb
Kurtshebayn <sbstn.snhz@gmail.com>
2024-05-07 17:54:17 -0300

    adding classes and def

---
+++ cuentaBancaria.py
@@ -0,0 +1,20 @@
+class Persona:
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+
+class Cliente(Persona):
+    pass
+
+
+def crear_cliente():
+    pass
+
+
+def inicio():
+    #crear cliente
+    #preguntar si quiere retirar,depositar o salir
+    #realizar accion
+    #mostrar balance
+    pass
